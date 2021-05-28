91e88d0aed0b9d57244cf26c308f4370401c8b87
nikhil545 <84938463+nikhil545@users.noreply.github.com>
2021-05-29 00:02:45 +0530

    Delete Website_1 directory

--- Website_1/public/JS/newactivity.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    var ar = ["Sleep", "Running", "Cycling", "Weight","Hydration"]
-    
-    if ($.inArray($("#inputactivity").val(), arr) == -1){
-        alert("Enter a vaalid activity type")
-    }
-
-})
